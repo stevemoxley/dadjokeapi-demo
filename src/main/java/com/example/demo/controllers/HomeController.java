1d4e2ead2cf44e14d894b6be0962fc057b3965bd
stevemoxley <sdmfyc@gmail.com>
2020-07-13 12:31:11 -0500

    Add blank starter project

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controllers;
 
-import com.example.demo.services.DadJoke;
-import com.example.demo.services.DadJokeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +13,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String Index(Model model) throws IOException {
-
-        DadJokeService dadJokeService = new DadJokeService();
-        DadJoke dadJoke = dadJokeService.GetDadJoke();
-
-        model.addAttribute("dadJoke", dadJoke);
-
         return "index";
     }
 
--- src/main/java/com/example/demo/services/DadJoke.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.services;
-
-public class DadJoke {
-    public String id;
-    public String joke;
-    public int status;
-}
